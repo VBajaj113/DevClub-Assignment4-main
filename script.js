da1c733b46ea08c94e39a6a2900e6d03ececf79b
Vaibhav Bajaj <bajajv2003@gmail.com>
2022-04-01 00:12:24 +0530

    tried a bit.. but no time

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 console.log("Loaded script.js");
+
+let summary = new XMLHttpRequest;
+summary.open("GET", "https://api.covid19api.com/summary");
+summary.send();
+
+let parsed_summary = JSON.parse(summary.responseText);
+
+let global_new = parsed_summary.Global.NewConfirmed;
