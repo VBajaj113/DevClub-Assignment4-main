0a5c9fcba1a413ba9f9f2a14b5c44252b149aff7
Vaibhav Bajaj <bajajv2003@gmail.com>
2022-03-31 08:42:02 +0530

    Main task done

--- Moodle+/login.js
+++ Moodle+/login.js
@@ -5,22 +5,21 @@ let arr = login_text.split("\n");
 
 let question = arr[3].split(" "); // Use split and array operations on the login_text string to extract the question
 
-let num1 = Number(question[question.length - 5]);
-let num2 = Number(question[question.length - 3]);
-let answer = ""; // Use if conditions to parse the question and calculate the answer. Make cases for all types of captcha asked
+let num1 = parseInt(question[question.length - 5]);
+let num2 = parseInt(question[question.length - 3]);
+let answer = 0; // Use if conditions to parse the question and calculate the answer. Make cases for all types of captcha asked
 
 if (question.includes("add")) {
-    answer = String(num1 + num2);
+    answer = num1 + num2;
 }
 else if (question.includes("subtract")) {
-    answer = String(num1 - num2);
+    answer = num1 - num2;
 }
 else if (question.includes("first")) {
-    answer = String(num1);
+    answer = num1;
 }
 else {
-    answer = String(num2);
+    answer = num2;
 }
 
-const captcha_input_element = document.getElementById("valuepkg3"); // Fill the selector for the captcha input element in ""
-captcha_input_element.value = answer;
+document.getElementById("valuepkg3").value = answer;
